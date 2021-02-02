fd9e8d4bda6345689ce3732023a70c89a6471b15
unknown <26983729+malcodeman@users.noreply.github.com>
2021-02-02 09:15:10 +0100

    Updates types in ThemeProvider component

--- src/ThemeProvider.tsx
+++ src/ThemeProvider.tsx
@@ -1,22 +1,18 @@
 import React from "react";
-import PropTypes from "prop-types";
-import { ThemeProvider } from "styled-components";
+import { DefaultTheme, ThemeProvider } from "styled-components";
 
 import { LightTheme as defaultTheme } from "./themes";
 
-function CustomThemeProvider(props) {
+type props = {
+  theme?: DefaultTheme;
+  children?: React.ReactNode;
+};
+
+function CustomThemeProvider(props: props) {
+  const { children } = props;
   const theme = { ...defaultTheme, ...props.theme };
 
-  return <ThemeProvider theme={theme}>{props.children}</ThemeProvider>;
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 }
-
-CustomThemeProvider.propTypes = {
-  theme: PropTypes.object,
-  children: PropTypes.node,
-};
-
-CustomThemeProvider.defaultProps = {
-  theme: {},
-};
 
 export default CustomThemeProvider;
