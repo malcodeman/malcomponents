fbda5f45abb4d59ba978f83272da701fb8c1bf2f
unknown <26983729+malcodeman@users.noreply.github.com>
2021-02-08 19:06:47 +0100

    Adds theming story

---
+++ stories/Theming.stories.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { withKnobs, select } from "@storybook/addon-knobs";
+
+import { LightTheme } from "../src/themes";
+
+import ThemeProvider from "../src/ThemeProvider";
+import Button from "../src/components/button/Button";
+
+export default { title: "Theming", decorators: [withKnobs] };
+
+export function Default(): React.ReactElement {
+  const buttonPrimaryFill = select(
+    "fillColor",
+    ["red", "green", "blue"],
+    "red"
+  );
+  const theme = {
+    ...LightTheme,
+    malcode: {
+      ...LightTheme.malcode,
+      colors: {
+        ...LightTheme.malcode.colors,
+        buttonPrimaryFill,
+      },
+    },
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Button>Button</Button>
+    </ThemeProvider>
+  );
+}
